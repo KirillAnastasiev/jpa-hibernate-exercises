b2c2e62b1f222c42f157f66d826b1f6adb0aca3d
KirillAnastasiev <kirillanastasiev1986@gmail.com>
2025-07-11 12:35:25 +0300

    account-dao: AccountDaoImpl.save() done

--- account-dao/src/main/java/com/bobocode/dao/AccountDaoImpl.java
+++ account-dao/src/main/java/com/bobocode/dao/AccountDaoImpl.java
@@ -1,20 +1,40 @@
 package com.bobocode.dao;
 
+import com.bobocode.exception.AccountDaoException;
 import com.bobocode.model.Account;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import java.util.List;
 
 public class AccountDaoImpl implements AccountDao {
+
     private EntityManagerFactory emf;
 
     public AccountDaoImpl(EntityManagerFactory emf) {
         this.emf = emf;
     }
 
+    /**
+     * Receives a new instance of {@link Account} and stores it into database. Sets a generated id to account.
+     *
+     * @param account new instance of account
+     */
     @Override
     public void save(Account account) {
-        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+
+            em.persist(account);
+
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            throw new AccountDaoException("Error save account: " + account, e);
+        } finally {
+            em.close();
+        }
     }
 
     @Override
