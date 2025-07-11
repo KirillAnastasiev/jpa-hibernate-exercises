d62345fd82fcf5a006af7d2fb075f3f269edc989
KirillAnastasiev <kirillanastasiev1986@gmail.com>
2025-07-11 16:54:40 +0300

    photo-comment-dao: Photo, PhotoComment done

--- photo-comment-dao/src/main/java/com/bobocode/model/Photo.java
+++ photo-comment-dao/src/main/java/com/bobocode/model/Photo.java
@@ -1,42 +1,42 @@
 package com.bobocode.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- * todo:
- * - implement no argument constructor
- * - implement getters and setters
- * - make a setter for field {@link Photo#comments} {@code private}
- * - implement equals() and hashCode() based on identifier field
- *
- * - configure JPA entity
- * - specify table name: "photo"
- * - configure auto generated identifier
- * - configure not nullable and unique column: url
- *
- * - initialize field comments
- * - map relation between Photo and PhotoComment on the child side
- * - implement helper methods {@link Photo#addComment(PhotoComment)} and {@link Photo#removeComment(PhotoComment)}
- * - enable cascade type {@link javax.persistence.CascadeType#ALL} for field {@link Photo#comments}
- * - enable orphan removal
- */
+@NoArgsConstructor
 @Getter
 @Setter
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "photo")
 public class Photo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "url", nullable = false, unique = true)
     private String url;
+
+    @Column(name = "description")
     private String description;
-    private List<PhotoComment> comments;
+
+    @Setter(AccessLevel.PRIVATE)
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "photo", orphanRemoval = true)
+    private List<PhotoComment> comments = new ArrayList<>();
 
     public void addComment(PhotoComment comment) {
-        throw new UnsupportedOperationException("Make me work!");
+        comments.add(comment);
+        comment.setPhoto(this);
     }
 
     public void removeComment(PhotoComment comment) {
-        throw new UnsupportedOperationException("Make me work!");
+        comments.remove(comment);
+        comment.setPhoto(null);
     }
 
 }
